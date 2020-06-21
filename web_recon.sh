491658c7ad7a87dc9cf322934f0d6db6e20d269d
lohcus <49207591+lohcus@users.noreply.github.com>
2020-06-21 15:29:36 -0300

    Update web_recon.sh

--- web_recon.sh
+++ web_recon.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #Criado por Daniel Domingues
+#Versao 1.2
 #https://github.com/lohcus
 
 array_diretorios=("")
+declare -a array_cont
 colunas=$(tput cols) # VERIFICA O TAMANHO DA JANELA PARA PODER DESENHAR O LAYOUT
 #=================================================================================================================
 
@@ -14,8 +16,9 @@ function uso() {
 	printf "\033[37;1mOpcoes:\n\033[m"
 	printf "\033[37;1m	-u url				URL base a ser pesquisada\n\033[m"
 	printf "\033[37;1m	-w wordlist			Wordlist utilizada na pesquisa\n\033[m"
+	printf "\033[37;1m	-a \"user-agent\"			String User Agent [opcional]\n\033[m"
 	printf "\033[37;1m	-x extensao			Pesquisa por extensao [opcional]\n\033[m"
-	printf "\033[37;1m	-d profundidade			Profundidade da pesquisa (min=1) [opcional]\n\033[m"
+	printf "\033[37;1m	-d profundidade			Profundidade da pesquisa (numerico, min=1) [opcional]\n\033[m"
 	divisao
 	exit 1
 }
@@ -23,6 +26,7 @@ function uso() {
 
 divisao () {
 	printf "\r\033[35;1m=\033[m"
+
 	# LACO PARA PREENCHER UMA LINHA COM "="
 	for i in $(seq 0 1 $(($colunas-2)))
 	do
@@ -32,10 +36,8 @@ divisao () {
 }
 #=================================================================================================================
 
-diretorios () { #PROCURA POR DIRETORIOS
-#	echo "curl -s -H 'User-Agent: DesecTool' -o /dev/null -w '%{http_code}' $1/$2/"
-	resposta=$(curl -s -H 'User-Agent: DesecTool' -o /dev/null -w '%{http_code}' $1/$2/)
-#	echo " - $resposta"
+diretorios () { # PROCURA POR DIRETORIOS
+	resposta=$(curl -s -A "$agent" -o /dev/null -w '%{http_code}' $1/$2/)
 	if [ $resposta == "200" ]
 	then
 		printf "\r\033[32;1m$3[+] Diretorio encontrado:\033[36;1m $1/$2\n\033[m"
@@ -43,17 +45,23 @@ diretorios () { #PROCURA POR DIRETORIOS
 		# CADA DIRETORIO EXISTENTE EH ADICIONADO A UM ARRAY PARA POSTERIOR PESQUISA DE ARQUIVOS NO MESMO
 		array_diretorios[$((${#array_diretorios[*]}+1))]=$(echo "$1/$2" | cut -d "/" -f 2-)
 		
-		# IDENTACAO DOS DIRETORIOS
-		espaco=$3
-		espaco=$(echo "$espaco-")
-
+		# CONTROLA A PROFUNDIDADE
 		if [ $contador -lt $deep ] 
 		then
 			contador=$((contador+1))
+			array_cont[$((${#array_cont[*]}+1))]=1
 			for diretorio in $(cat $wordlist)
 			do	
+				# IDENTACAO DOS DIRETORIOS
+				espaco=$3
+				espaco=$(echo "$espaco-")
+				cor=$((30+${#array_cont[*]}))
+				
+				printf "\r\033[$cor;1m$espaco>${array_cont[${#array_cont[*]}]} de $nomes nomes em \033[36;1m/$2     \033[m" # CONTADOR DE NOMES
 				diretorios "$1/$2" $diretorio $espaco
-			done		
+				array_cont[${#array_cont[*]}]=$((${array_cont[${#array_cont[*]}]}+1))
+			done
+			unset array_cont[${#array_cont[*]}] # APAGA A ULTIMA POSICAO DO ARRAY CONTADOR PARA PODER RETOMAR O CONTADOR ANTERIOR
 		fi
 	fi
 }
@@ -61,17 +69,13 @@ diretorios () { #PROCURA POR DIRETORIOS
 
 arquivos () { # PROCURA POR ARQUIVOS
 	# PESQUISA PELOS NOMES DA WORDLIST SEM EXTENSAO
-#	echo -n "curl -s -H 'User-Agent: DesecTool' -o /dev/null -w '%{http_code}' $1/$2"
-	resposta=$(curl -s -H 'User-Agent: DesecTool' -o /dev/null -w '%{http_code}' $1/$2)
-#	echo " - $resposta"
+	resposta=$(curl -s -A "$agent" -o /dev/null -w '%{http_code}' $1/$2)
 	if [ $resposta == "200" ]
 	then
 		printf "\r\033[33;1m$4|- Arquivo encontrado:\033[37;1m $1/$2\n\033[m"
 	fi
 	# PESQUISA PELOS NOMES DA WORDLIST COM EXTENSAO
-#	echo -n "curl -s -H 'User-Agent: DesecTool' -o /dev/null -w '%{http_code}' $1/$2.$3"
-	resposta=$(curl -s -H 'User-Agent: DesecTool' -o /dev/null -w '%{http_code}' $1/$2.$3)
-#	echo " - $resposta"
+	resposta=$(curl -s -A "$agent" -o /dev/null -w '%{http_code}' $1/$2.$3)
 	if [ $resposta == "200" ]
 	then
 		printf "\r\033[33;1m$4|- Arquivo $3 encontrado:\033[37;1m $1/$2.$3\n\033[m"
@@ -90,7 +94,11 @@ tput cup 0 $centro_coluna #POSICIONAR O CURSOR
 printf "\033[37;1mLOHCUS WEB RECON\n\033[m"
 
 deep=9999
-while getopts "hu:w:x:d:" OPTION
+agent="LohcusWeb"
+cor=31
+
+# VERIFICA AS OPCOES DIGITADAS
+while getopts "hu:w:x:d:a:" OPTION
 do
 	case $OPTION in
     	"h") uso
@@ -105,14 +113,18 @@ do
         	;;
       	"d") deep=$OPTARG
         	;;
+		"a") agent=$OPTARG
+        	;;
       	"?") uso
         	;;
    esac
 done
 shift $((OPTIND-1))
 
+# VERIFICA SE FORAM DIGITADOS OS PARAMETROS OBRIGATORIOS -u E -w
 [ -z "$url" -o -z "$wordlist" ] && uso
 
+# VERIFICA SE O PARAMETRO -d EH NUMERICO
 numeric='^[0-9]+$'
 if ! [[ $deep =~ $numeric ]] ; then
    	printf "\033[31;1m[-] A profundidade deve ser numeria maior que 0!\n\033[m"
@@ -127,7 +139,7 @@ then
 fi
 
 # VERIFICA SE A URL NAO EXISTE OU NAO ESTA RESPONDENDO
-status=$(curl -s -H 'User-Agent: DesecTool' -I $url)
+status=$(curl -s -A "$agent" -I $url)
 if [ "$status" == "" ]
 then
 	printf "\033[31;1m[-] Verifique a URL digitada!\n\033[m"
@@ -148,22 +160,17 @@ printf "\033[37;1m[+] Buscando por Diretorios\n\033[m"
 divisao
 
 # BUSCA POR DIRETORIOS
+array_cont[0]=1
 cont=1
 for diretorio in $(cat $wordlist)
 do
 	contador=1
-	# CONTADOR DE NOMES
-	printf "\r \033[31;1m$cont de $nomes nomes\033[m"
-
+	printf "\r\033[31;1m>${array_cont[0]} de $nomes nomes em \033[36;1m/     \033[m" # CONTADOR DE NOMES
 	diretorios $url $diretorio ""
-
-	cont=$(($cont+1))
-	
+	array_cont[0]=$((${array_cont[0]}+1))
 done
 
 divisao
-
-#echo ${array_diretorios[*]}
 
 # SE FOR DIGITADO O TERCEIRO PARAMETRO (EXTENSAO) PESQUISA POR ARQUIVOS NA RAIZ E EM CADA DIRETORIO ENCONTRADO, UTILIZANDO A MESMA WORDLIST
 if [ ! -z $extensao ]
@@ -173,17 +180,14 @@ do
 	printf "\033[37;1m[+] Buscando por Arquivos em \033[36;1m$1/$elemento\n\033[m"
 	cont=1
 	for arquivo in $(cat $wordlist)
-	do
-		# CONTADOR DE NOMES
-		printf "\r \033[31;1m$cont de $nomes nomes\033[m"
-		
+	do		
+		printf "\r\033[31;1m>$cont de $nomes nomes\033[m" # CONTADOR DE NOMES	
 		if [ -z "$elemento" ]
 		then
 			arquivos "$url" $arquivo $extensao " "
 		else
 			arquivos "$url/$elemento" $arquivo $extensao " "
 		fi
-		
 		cont=$(($cont+1))
 	done
 	divisao
